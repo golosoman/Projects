015861ed365850eb5667037de38dc7466f857feb
Golosoman <constan.lapin2015@mail.ru>
2024-04-07 18:39:28 +0400

    reformat code

--- src/main/java/com/example/MedicalCalculators/MedicalCalculatorsApplication.java
+++ src/main/java/com/example/MedicalCalculators/MedicalCalculatorsApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MedicalCalculatorsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MedicalCalculatorsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MedicalCalculatorsApplication.class, args);
+    }
 
 }
