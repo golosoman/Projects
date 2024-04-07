77e62d5ecc3fe1335ad2161541c3192f40e353c6
Golosoman <constan.lapin2015@mail.ru>
2024-04-07 18:36:51 +0400

    delete old version exception

--- src/main/java/com/example/MedicalCalculators/exceptions/CalculatorAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.example.MedicalCalculators.exceptions;
-
-public class CalculatorAlreadyExistsException extends Exception{
-    public CalculatorAlreadyExistsException(String message){
-        super(message);
-    }
-}
