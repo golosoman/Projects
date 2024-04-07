febb803294cb6226012f7133cfa0800748fdadc8
Golosoman <constan.lapin2015@mail.ru>
2024-04-07 18:38:23 +0400

    edit calculator controller and service

--- src/main/java/com/example/MedicalCalculators/service/CalculatorService.java
+++ src/main/java/com/example/MedicalCalculators/service/CalculatorService.java
@@ -1,8 +1,9 @@
 package com.example.MedicalCalculators.service;
 
 import com.example.MedicalCalculators.entity.CalculatorEntity;
-import com.example.MedicalCalculators.exceptions.CalculatorAlreadyExistsException;
-import com.example.MedicalCalculators.exceptions.CalculatorNotFoundException;
+import com.example.MedicalCalculators.exceptions.AlreadyExistsException;
+import com.example.MedicalCalculators.exceptions.NotFoundException;
+import com.example.MedicalCalculators.model.Calculator;
 import com.example.MedicalCalculators.repository.CalculatorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,34 +12,31 @@ import org.springframework.stereotype.Service;
 public class CalculatorService {
     @Autowired
     private CalculatorRepository calculatorRepository;
-    public CalculatorEntity add(CalculatorEntity calculator) throws CalculatorAlreadyExistsException {
-        if(calculatorRepository.findByName(calculator.getName()) != null){
-            throw new CalculatorAlreadyExistsException("Калькулятор с таким именем уже существует");
+
+    public CalculatorEntity add(CalculatorEntity calculator) throws AlreadyExistsException {
+        if (calculatorRepository.findByName(calculator.getName()) != null) {
+            throw new AlreadyExistsException("Калькулятор с таким именем уже существует");
         }
         return calculatorRepository.save(calculator);
     }
 
-    public CalculatorEntity getOne(Long id) throws CalculatorNotFoundException {
+    public Calculator getOne(Long id) throws NotFoundException {
         CalculatorEntity calculator = calculatorRepository.findById(id).get();
-        if(calculator == null){
-            throw new CalculatorNotFoundException("Калькулятор не найден");
+        if (calculator == null) {
+            throw new NotFoundException("Калькулятор не найден");
         }
-        return calculator;
+        return Calculator.toModel(calculator);
     }
-
-//    public CalculatorEntity getByName(String name) throws CalculatorNotFoundException {
-//        CalculatorEntity calculator = calculatorRepo.findByName(name);
-//        if(calculator == null){
-//            throw new CalculatorNotFoundException("Калькулятор не найден");
-//        }
-//        return calculator;
-//    }
 
     public Iterable<CalculatorEntity> getAll() {
         return calculatorRepository.findAll();
     }
 
-    public Long delete(Long id){
+    public Long delete(Long id) throws NotFoundException {
+        CalculatorEntity calculator = calculatorRepository.findById(id).get();
+        if (calculator == null) {
+            throw new NotFoundException("Калькулятор не найден");
+        }
         calculatorRepository.deleteById(id);
         return id;
     }
