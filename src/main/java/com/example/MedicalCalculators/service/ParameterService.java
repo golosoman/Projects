7c9d6cb0c2cbfc55fe3c356ff6c68b541b29c0f6
Golosoman <constan.lapin2015@mail.ru>
2024-04-07 23:27:17 +0400

    edit methods for return model Parameter

--- src/main/java/com/example/MedicalCalculators/service/ParameterService.java
+++ src/main/java/com/example/MedicalCalculators/service/ParameterService.java
@@ -4,6 +4,8 @@ import com.example.MedicalCalculators.entity.CalculatorEntity;
 import com.example.MedicalCalculators.entity.ParameterEntity;
 import com.example.MedicalCalculators.exceptions.AlreadyExistsException;
 import com.example.MedicalCalculators.exceptions.NotFoundException;
+import com.example.MedicalCalculators.model.Calculator;
+import com.example.MedicalCalculators.model.Parameter;
 import com.example.MedicalCalculators.repository.ParameterRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -20,16 +22,16 @@ public class ParameterService {
         return parameterRepository.save(parameter);
     }
 
-    public ParameterEntity getOne(Long id) throws NotFoundException {
+    public Parameter getOne(Long id) throws NotFoundException {
         ParameterEntity parameter = parameterRepository.findById(id).get();
         if (parameter == null) {
             throw new NotFoundException("Параметр не найден");
         }
-        return parameter;
+        return Parameter.toModel(parameter);
     }
 
-    public Iterable<ParameterEntity> getAll() {
-        return parameterRepository.findAll();
+    public Iterable<Parameter> getAll() {
+        return Parameter.toModelList(parameterRepository.findAll());
     }
 
     public Long delete(Long id) throws NotFoundException {
