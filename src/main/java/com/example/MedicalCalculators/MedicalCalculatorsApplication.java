0d8e806a3773eef8719934b00f1f46d800607ac4
Golosoman <constan.lapin2015@mail.ru>
2024-04-07 17:39:53 +0400

    Create project

---
+++ src/main/java/com/example/MedicalCalculators/MedicalCalculatorsApplication.java
@@ -0,0 +1,13 @@
+package com.example.MedicalCalculators;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalCalculatorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalCalculatorsApplication.class, args);
+	}
+
+}
