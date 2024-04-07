e6f8e68d23dafac7271f34a52f78cfc0f9a349eb
Golosoman <constan.lapin2015@mail.ru>
2024-04-07 17:52:48 +0400

    Add exceptions for response(already exists and not found)

---
+++ src/main/java/com/example/MedicalCalculators/exceptions/CalculatorNotFoundException.java
@@ -0,0 +1,7 @@
+package com.example.MedicalCalculators.exceptions;
+
+public class CalculatorNotFoundException extends Exception{
+    public CalculatorNotFoundException(String message){
+        super(message);
+    }
+}
