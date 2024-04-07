a0117e126941ea93ee287ca61963aeb39c2e784f
Golosoman <constan.lapin2015@mail.ru>
2024-04-07 23:26:11 +0400

    add Parameter and ParameterController

---
+++ src/main/java/com/example/MedicalCalculators/model/Parameter.java
@@ -0,0 +1,33 @@
+package com.example.MedicalCalculators.model;
+
+import com.example.MedicalCalculators.entity.CalculatorEntity;
+import com.example.MedicalCalculators.entity.ParameterEntity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class Parameter {
+    Long id;
+    String name;
+    public static Parameter toModel(ParameterEntity entity) {
+        Parameter model = new Parameter();
+        model.setId(entity.getId());
+        model.setName(entity.getName());
+        return model;
+    }
+
+    public static List<Parameter> toModelList(Iterable<ParameterEntity> entities) {
+        List<Parameter> list = new ArrayList<Parameter>();
+        entities.forEach((entity) -> {
+            Parameter model = Parameter.toModel(entity);
+            list.add(model);
+        });
+        return list;
+    }
+}
