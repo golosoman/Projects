892759c4ab96b2b64e0a6cab2a16b54da1b08b1b
Golosoman <constan.lapin2015@mail.ru>
2024-04-13 09:45:48 +0400

    add new model CalculatorInfo, edit import in Calculator, add getInfo and getResult for CalculatorController

--- src/main/java/com/example/MedicalCalculators/api/controller/CalculatorController.java
+++ src/main/java/com/example/MedicalCalculators/api/controller/CalculatorController.java
@@ -3,10 +3,13 @@ package com.example.MedicalCalculators.api.controller;
 import com.example.MedicalCalculators.entity.CalculatorEntity;
 import com.example.MedicalCalculators.exceptions.AlreadyExistsException;
 import com.example.MedicalCalculators.exceptions.NotFoundException;
-import com.example.MedicalCalculators.service.CalculatorService;
+import com.example.MedicalCalculators.exceptions.ParameterException;
+import com.example.MedicalCalculators.service.CalculatorService.CalculatorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/calculator")
@@ -50,6 +53,28 @@ public class CalculatorController {
     public ResponseEntity delete(@PathVariable Long id) {
         try {
             return ResponseEntity.ok(calculatorService.delete(id));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body("Произошла ошибка");
+        }
+    }
+
+    @GetMapping("/{name}/info")
+    public ResponseEntity getInfo(@PathVariable String name){
+        try{
+            return ResponseEntity.ok(calculatorService.getInfo(name));
+        } catch (NotFoundException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body("Произошла ошибка");
+        }
+    }
+
+    @PostMapping("/{name}/result")
+    public ResponseEntity getResult(@PathVariable String name, @RequestBody Map<String, String> parameters){
+        try{
+            return ResponseEntity.ok(calculatorService.getResult(name, parameters));
+        } catch (NotFoundException | ParameterException  e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
         } catch (Exception e) {
             return ResponseEntity.badRequest().body("Произошла ошибка");
         }
