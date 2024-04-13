d339b78ae9eb46b5469aa496bbfb042889f9eee7
Golosoman <constan.lapin2015@mail.ru>
2024-04-13 12:04:12 +0400

    delete parameters

--- src/main/java/com/example/MedicalCalculators/repository/ParameterRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.MedicalCalculators.repository;
-
-import com.example.MedicalCalculators.entity.CalculatorEntity;
-import com.example.MedicalCalculators.entity.ParameterEntity;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ParameterRepository extends CrudRepository<ParameterEntity, Long> {
-    ParameterEntity findByName(String name);
-}
