fc34642aab26d9c0973e53002bb259dd855ed3c9
Golosoman <constan.lapin2015@mail.ru>
2024-04-16 19:35:04 +0400

    delete old and unnecessary data

--- src/main/java/com/example/MedicalCalculators/repository/CalculatorRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.MedicalCalculators.repository;
-
-import com.example.MedicalCalculators.entity.CalculatorEntity;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CalculatorRepository extends CrudRepository<CalculatorEntity, Long> {
-    CalculatorEntity findByName(String name);
-}
