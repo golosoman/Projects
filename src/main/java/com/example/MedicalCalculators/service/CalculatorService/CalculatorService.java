6e2d448c4adeef3cab039a5e472b0cbd54d46577
Golosoman <constan.lapin2015@mail.ru>
2024-04-16 19:34:17 +0400

    edit CalculatorController and CalculatorService

--- src/main/java/com/example/MedicalCalculators/service/CalculatorService/CalculatorService.java
+++ src/main/java/com/example/MedicalCalculators/service/CalculatorService/CalculatorService.java
@@ -1,73 +1,127 @@
 package com.example.MedicalCalculators.service.CalculatorService;
 
-import com.example.MedicalCalculators.entity.CalculatorEntity;
+//import com.example.MedicalCalculators.entity.CalculatorEntity;
+import com.example.MedicalCalculators.dto.request.BMICalculatorRequest;
+import com.example.MedicalCalculators.dto.request.RIDDCalculatorRequest;
+import com.example.MedicalCalculators.dto.request.TitrationCalculatorRequest;
+import com.example.MedicalCalculators.dto.response.CalculatorID;
+import com.example.MedicalCalculators.dto.response.CalculatorInfoFull;
+import com.example.MedicalCalculators.dto.response.CalculatorInfo;
+import com.example.MedicalCalculators.dto.request.CalculatorInfoRequest;
+import com.example.MedicalCalculators.dto.response.CalculatorResult;
 import com.example.MedicalCalculators.exceptions.AlreadyExistsException;
 import com.example.MedicalCalculators.exceptions.NotFoundException;
-import com.example.MedicalCalculators.exceptions.ParameterException;
-import com.example.MedicalCalculators.model.calculator.Calculator;
-import com.example.MedicalCalculators.model.calculator.CalculatorInfo;
+//import com.example.MedicalCalculators.repository.CalculatorRepository;
 import com.example.MedicalCalculators.service.CalculatorService.typeCalculator.CalculatorBodyMassIndex;
 import com.example.MedicalCalculators.service.CalculatorService.typeCalculator.CalculatorRateIntravenousDripDrug;
 import com.example.MedicalCalculators.service.CalculatorService.typeCalculator.CalculatorTitrations;
-import com.example.MedicalCalculators.model.result.Result;
-import com.example.MedicalCalculators.repository.CalculatorRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Map;
+import java.util.*;
 
 @Service
 public class CalculatorService {
-    @Autowired
-    private CalculatorRepository calculatorRepository;
+    private static final List<CalculatorInfoFull> CALCULATOR_LIST = new ArrayList<>();
 
-    public CalculatorEntity add(CalculatorEntity calculator) throws AlreadyExistsException {
-        if (calculatorRepository.findByName(calculator.getName()) != null) {
-            throw new AlreadyExistsException("Калькулятор с таким именем уже существует");
-        }
-        return calculatorRepository.save(calculator);
+    static {
+        CALCULATOR_LIST.add(new CalculatorInfoFull(1L, "BodyMassIndex", "Petrov"));
+        CALCULATOR_LIST.add(new CalculatorInfoFull(2L, "RateIntravenousDripDrug", "Ivanov"));
+        CALCULATOR_LIST.add(new CalculatorInfoFull(3L, "Titrations", "Sidorov"));
     }
 
-    public Calculator getOne(Long id) throws NotFoundException {
-        CalculatorEntity calculator = calculatorRepository.findById(id).get();
-        if (calculator == null) {
-            throw new NotFoundException("Калькулятор не найден");
+    private static CalculatorInfoFull findCalculatorByName(String name) {
+        for (CalculatorInfoFull calculatorInfoFull : CALCULATOR_LIST) {
+            if (calculatorInfoFull.getName().equals(name)) {
+                return calculatorInfoFull;
+            }
         }
-        return Calculator.toModel(calculator);
+        return null;
     }
 
-    public Iterable<Calculator> getAll() {
-        return Calculator.toModelList(calculatorRepository.findAll());
+    private static CalculatorInfoFull findCalculatorById(Long id) {
+        for (CalculatorInfoFull calculatorInfoFull : CALCULATOR_LIST) {
+            if (calculatorInfoFull.getId().equals(id)) {
+                return calculatorInfoFull;
+            }
+        }
+        return null;
     }
 
-    public CalculatorInfo getInfo(String name) throws NotFoundException {
-        CalculatorEntity calculator = calculatorRepository.findByName(name);
-        if (calculator == null) {
-            throw new NotFoundException("Калькулятор не найден");
+    public static Long getLastIdInList() {
+        if (CALCULATOR_LIST.isEmpty()) {
+            return 0L;
         }
-        return CalculatorInfo.toModel(calculator);
+        CalculatorInfoFull lastCalculatorInfoFull = CALCULATOR_LIST.get(CALCULATOR_LIST.size() - 1);
+        return lastCalculatorInfoFull.getId();
     }
 
-    public Long delete(Long id) throws NotFoundException {
-        CalculatorEntity calculator = calculatorRepository.findById(id).get();
-        if (calculator == null) {
-            throw new NotFoundException("Калькулятор не найден");
+    public CalculatorInfoFull getOne(Long id){
+        CalculatorInfoFull calculatorInfoFull = findCalculatorById(id);
+        if (calculatorInfoFull == null){
+            throw new NotFoundException("Calculator with ID " + id + " not found");
         }
-        calculatorRepository.deleteById(id);
-        return id;
+        return calculatorInfoFull;
     }
 
-    public Result getResult(String name, Map<String, String> parameters) throws NotFoundException, ParameterException {
-        CalculatorEntity calculator = calculatorRepository.findByName(name);
-        if (calculator == null) {
-            throw new NotFoundException("Калькулятор не найден");
+    public CalculatorInfoFull add(CalculatorInfoRequest calculatorInfoRequest){
+        if (findCalculatorByName(calculatorInfoRequest.getName()) != null){
+            throw new AlreadyExistsException("Calculator with name "
+                    + calculatorInfoRequest.getName()
+                    + " alreadyExists");
         }
-        return switch (name){
-            case "calculatorTitrationRate" -> (new CalculatorTitrations()).calculate(parameters);
-            case "calculatorBodyMassIndex" -> (new CalculatorBodyMassIndex()).calculate(parameters);
-            case "calculatorRateIntravenousDripDrug" -> (new CalculatorRateIntravenousDripDrug()).calculate(parameters);
-            default -> throw new NotFoundException("Калькулятор не найден");
-        };
+        CalculatorInfoFull calculatorInfoFull = new CalculatorInfoFull(
+                getLastIdInList() + 1,
+                calculatorInfoRequest.getName(),
+                calculatorInfoRequest.getDescription());
+        CALCULATOR_LIST.add(calculatorInfoFull);
+        return calculatorInfoFull;
+    }
+
+    public List<CalculatorInfoFull> getAll() {
+        return CALCULATOR_LIST;
+    }
+
+    public CalculatorInfo getInfo(String name) {
+        CalculatorInfoFull calculatorInfoFull = findCalculatorByName(name);
+        if (calculatorInfoFull == null){
+            throw new NotFoundException("Calculator with name " + name + " not found");
+        }
+        return new CalculatorInfo(calculatorInfoFull.getDescription());
+
+    }
+
+    public CalculatorInfoFull delete(Long id) {
+        Iterator<CalculatorInfoFull> iterator = CALCULATOR_LIST.iterator();
+        while (iterator.hasNext()) {
+            CalculatorInfoFull calculatorInfoFull = iterator.next();
+            if (calculatorInfoFull.getId().equals(id)) {
+                iterator.remove(); // Удаляем элемент, если его Id совпадает с заданным
+                return calculatorInfoFull;
+            }
+        }
+        throw new NotFoundException("Calculator with ID " + id + " not found");
+    }
+
+    public CalculatorInfoFull update(Long id, CalculatorInfoRequest calculatorInfoRequest){
+        CalculatorInfoFull calculatorInfoFull = findCalculatorById(id);
+        if (calculatorInfoFull == null){
+            throw new NotFoundException("Calculator with ID " + id + " not found");
+        }
+        calculatorInfoFull.setName(calculatorInfoRequest.getName());
+        calculatorInfoFull.setDescription(calculatorInfoRequest.getDescription());
+        return calculatorInfoFull;
+    }
+
+    public CalculatorResult getBMIResult(BMICalculatorRequest calculatorRequest){
+        return new CalculatorBodyMassIndex().calculate(calculatorRequest);
+    }
+
+    public CalculatorResult getTitrationResult(TitrationCalculatorRequest calculatorRequest){
+        return new CalculatorTitrations().calculate(calculatorRequest);
+    }
+
+    public CalculatorResult getRIDDResult(RIDDCalculatorRequest calculatorRequest){
+        return new CalculatorRateIntravenousDripDrug().calculate(calculatorRequest);
     }
 
 }
