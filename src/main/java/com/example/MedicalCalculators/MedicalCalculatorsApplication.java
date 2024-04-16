f17ffe82428a51e7cb0e82817e8f2242d6487a86
Golosoman <constan.lapin2015@mail.ru>
2024-04-16 20:51:42 +0400

    reformat code

--- src/main/java/com/example/MedicalCalculators/MedicalCalculatorsApplication.java
+++ src/main/java/com/example/MedicalCalculators/MedicalCalculatorsApplication.java
@@ -2,8 +2,9 @@ package com.example.MedicalCalculators;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class MedicalCalculatorsApplication {
 
     public static void main(String[] args) {
