6b45a2291a7a3460ccc3e445bbab8a4355f81d16
Golosoman <constan.lapin2015@mail.ru>
2024-04-22 15:20:50 +0400

    reformat code

--- src/main/java/com/example/MedicalCalculators/exceptions/ExceptionApiHandler.java
+++ src/main/java/com/example/MedicalCalculators/exceptions/ExceptionApiHandler.java
@@ -3,7 +3,6 @@ package com.example.MedicalCalculators.exceptions;
 import com.example.MedicalCalculators.exceptions.ErrorMessage;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
