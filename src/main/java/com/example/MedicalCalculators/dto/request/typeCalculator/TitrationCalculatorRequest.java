f51f7978a74828f28788c409716c743f908c492b
Golosoman <constan.lapin2015@mail.ru>
2024-04-22 17:19:20 +0400

    add description fro Medical Calculators API

--- src/main/java/com/example/MedicalCalculators/dto/request/typeCalculator/TitrationCalculatorRequest.java
+++ src/main/java/com/example/MedicalCalculators/dto/request/typeCalculator/TitrationCalculatorRequest.java
@@ -1,6 +1,7 @@
 package com.example.MedicalCalculators.dto.request.typeCalculator;
 
 import com.example.MedicalCalculators.dto.request.typeCalculator.BaseCalculatorRequest;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,13 +9,15 @@ import lombok.Setter;
 @Setter
 @Getter
 @AllArgsConstructor
+@Schema(description = "Объект запроса калькулятора для расчета скорости инфузии " +
+        "препарата через линеомат (скорость титрования)")
 public class TitrationCalculatorRequest extends BaseCalculatorRequest {
-    // Вес пациента, кг
+    @Schema(description = "Вес человека в кг", example = "65")
     private Double weightPatient;
-    // Дозировка в мкг*кг/мин или мл/час
+    @Schema(description = "Дозировка препарата в мкг*кг/мин или мл/час", example = "2")
     private Double dosage;
-    // Количество препарата, мг
+    @Schema(description = "Количество препарата в мг", example = "10")
     private Double amountOfDrug;
-    // Общий объем раствора, мл
+    @Schema(description = "Общий объем раствора в мл", example = "50")
     private Double volumeOfSolution;
 }
--- src/main/java/com/example/MedicalCalculators/exceptions/ErrorMessage.java
+++ src/main/java/com/example/MedicalCalculators/exceptions/ErrorMessage.java
@@ -1,5 +1,6 @@
 package com.example.MedicalCalculators.exceptions;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -7,9 +8,15 @@ import java.util.Date;
 
 @Data
 @AllArgsConstructor
+@Schema(description = "Объект сообщения об ошибке")
 public class ErrorMessage {
+    @Schema(description = "Код состояния http", example = "404")
     private int statusCode;
+    @Schema(description = "Дата и время, в которое произашла ошибка", example = "2024-04-22T17:01:56.160+04:00")
     private Date timestamp;
+    @Schema(description = "Сообщение об ошибке", example = "Calculator with ID 5 not found")
     private String message;
+    @Schema(description = "Описание запроса", example = "Resolved [com.example.MedicalCalculators." +
+            "exceptions.NotFoundException: Calculator with ID 5 not found]")
     private String description;
 }
