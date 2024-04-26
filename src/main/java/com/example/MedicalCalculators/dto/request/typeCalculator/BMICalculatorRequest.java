00995a7493029e106b4e266310a4ba95e738b1a0
Golosoman <constan.lapin2015@mail.ru>
2024-04-26 19:58:55 +0400

    added parameters for dto validation

--- src/main/java/com/example/MedicalCalculators/dto/request/typeCalculator/BMICalculatorRequest.java
+++ src/main/java/com/example/MedicalCalculators/dto/request/typeCalculator/BMICalculatorRequest.java
@@ -1,9 +1,9 @@
 package com.example.MedicalCalculators.dto.request.typeCalculator;
 
-import com.example.MedicalCalculators.dto.request.typeCalculator.BaseCalculatorRequest;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -11,13 +11,14 @@ import lombok.Setter;
 @Setter
 @Getter
 @AllArgsConstructor
-@Schema(description = "Объект запроса калькулятора индекса массы тела")
+@Schema(description = "Объект, включающий параметры для калькулятора индекса массы тела")
 public class BMICalculatorRequest extends BaseCalculatorRequest {
     @Schema(description = "Вес человека в кг", example = "65")
-    @Min(10)
+    @Min(value = 10, message = "The minimum weight value is 10 kg")
     private Double weightPatient;
-    @Min(1)
-    @Max(300)
+
     @Schema(description = "Рост человека в см", example = "176")
+    @Positive(message = "The growth value is strictly positive")
+    @Max(value = 300, message = "The height cannot exceed 300 cm")
     private Double height;
 }
