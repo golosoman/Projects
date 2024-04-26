3226239ee94b6490d38d793d00335dfa076906f3
Golosoman <constan.lapin2015@mail.ru>
2024-04-26 19:58:55 +0400

    added parameters for dto validation

--- src/main/java/com/example/MedicalCalculators/dto/request/typeCalculator/RIDDCalculatorRequest.java
+++ src/main/java/com/example/MedicalCalculators/dto/request/typeCalculator/RIDDCalculatorRequest.java
@@ -1,7 +1,7 @@
 package com.example.MedicalCalculators.dto.request.typeCalculator;
 
-import com.example.MedicalCalculators.dto.request.typeCalculator.BaseCalculatorRequest;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,13 +9,17 @@ import lombok.Setter;
 @Setter
 @Getter
 @AllArgsConstructor
-@Schema(description = "Объект запроса калькулятора для расчета скорости внутривенного " +
+@Schema(description = "Объект, включающий параметры для калькулятора расчета скорости внутривенного " +
         "капельного введения препарата")
 public class RIDDCalculatorRequest extends BaseCalculatorRequest {
     @Schema(description = "Объем раствора в мл", example = "60")
+    @Positive(message = "The value of the solution volume is strictly positive")
     private Double volumeOfSolution;
+
     @Schema(description = "Желаемое время введения препарата в минутах/часах", example = "30")
+    @Positive(message = "The value of the desired drug administration time is strictly positive")
     private Double timeTaking;
+
     @Schema(description = "Время в минутах?", example = "true")
     private Boolean isMinute;
 }
--- src/main/java/com/example/MedicalCalculators/dto/request/typeCalculator/TitrationCalculatorRequest.java
+++ src/main/java/com/example/MedicalCalculators/dto/request/typeCalculator/TitrationCalculatorRequest.java
@@ -1,7 +1,7 @@
 package com.example.MedicalCalculators.dto.request.typeCalculator;
 
-import com.example.MedicalCalculators.dto.request.typeCalculator.BaseCalculatorRequest;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,17 +9,25 @@ import lombok.Setter;
 @Setter
 @Getter
 @AllArgsConstructor
-@Schema(description = "Объект запроса калькулятора для расчета скорости инфузии " +
+@Schema(description = "Объект, включающий параметры для калькулятора расчета скорости инфузии " +
         "препарата через линеомат (скорость титрования)")
 public class TitrationCalculatorRequest extends BaseCalculatorRequest {
     @Schema(description = "Вес человека в кг", example = "65")
+    @Positive(message = "The weight value is strictly positive")
     private Double weightPatient;
+
     @Schema(description = "Дозировка препарата в мкг*кг/мин или мл/час", example = "2")
+    @Positive(message = "The dosage value of the drug is strictly positive")
     private Double dosage;
+
     @Schema(description = "Количество препарата в мг", example = "10")
+    @Positive(message = "The value of the amount of the drug is strictly positive")
     private Double amountOfDrug;
+
     @Schema(description = "Общий объем раствора в мл", example = "50")
+    @Positive(message = "The value of the total volume of the solution is strictly positive")
     private Double volumeOfSolution;
+
     @Schema(description = "Дозировка в мл/час?", example = "true")
     private Boolean isMlInHour;
 }
