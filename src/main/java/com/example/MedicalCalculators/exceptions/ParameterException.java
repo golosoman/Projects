d2cec4b36a83707a0d85eb401a5e0663f06e98ca
Golosoman <constan.lapin2015@mail.ru>
2024-04-26 19:56:57 +0400

    moved exceptions to field api

--- src/main/java/com/example/MedicalCalculators/exceptions/ParameterException.java
+++
@@ -1,7 +0,0 @@
-package com.example.MedicalCalculators.exceptions;
-
-public class ParameterException extends IllegalArgumentException {
-    public ParameterException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/example/MedicalCalculators/exceptions/api/NotFoundException.java
+++ src/main/java/com/example/MedicalCalculators/exceptions/api/NotFoundException.java
@@ -1,6 +1,5 @@
-package com.example.MedicalCalculators.exceptions;
+package com.example.MedicalCalculators.exceptions.api;
 
-import java.io.Serial;
 
 public class NotFoundException extends RuntimeException {
     public NotFoundException(String message) {
