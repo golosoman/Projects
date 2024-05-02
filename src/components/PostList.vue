5fe4b4017808bfcef2389c2b2d023d63607040d9
Golosmanchick <constan.lapin2015@mail.ru>
2024-05-02 23:41:24 +0400

    add 2 component - Post.vue and PostList.vue

---
+++ src/components/PostList.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { defineProps } from 'vue'
+import Post from '@/components/Post.vue'
+
+const props = defineProps({
+  postList: {
+    type: Array,
+    require: true
+  }
+})
+</script>
+
+<template>
+  <div class="posts_content" v-for="(post, index) in props.postList" :key="index">
+    <Post :post="post" />
+  </div>
+</template>
+
+<style scoped></style>
