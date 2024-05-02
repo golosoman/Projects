dff4cab90af9911370a8f2be034b11919c201af1
Golosmanchick <constan.lapin2015@mail.ru>
2024-05-02 23:43:09 +0400

    add pages MainPage.vue PostIdPage.vue PostsPage.vue

---
+++ src/pages/PostsPage.vue
@@ -0,0 +1,21 @@
+<script>
+import PostList from '@/components/PostList.vue'
+import { usePosts } from '@/hooks/usePosts.js'
+
+export default {
+  components: {
+    PostList
+  },
+  setup() {
+    const posts = usePosts()
+    return posts
+  }
+}
+</script>
+<template>
+  <div class="content">
+    <PostList :post-list="posts" />
+  </div>
+</template>
+
+<style scoped></style>
