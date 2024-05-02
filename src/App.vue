88256b7bb8b82d8790c6354a24722de605c4a837
Golosmanchick <constan.lapin2015@mail.ru>
2024-05-02 23:44:40 +0400

    edit App.vue and main.js

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+// import { onMounted, ref } from 'vue'
+// import PostList from './components/PostList.vue'
+
+// import commentsList from './data/comments.json'
+// import userList from './data/users.json'
+
+// let comments = ref([])
+// let users = ref([])
+
+// onMounted(() => {
+//   comments.value = commentsList
+//   users.value = userList
+// })
+</script>
+
+<template>
+  <template>
+    <div class="app">
+      <router-view></router-view>
+    </div>
+  </template>
+</template>
+
+<style scoped></style>
