fdac2d6f71ff93f4936626ef94610643ad868e05
Golosmanchick <constan.lapin2015@mail.ru>
2024-05-05 23:50:40 +0400

    edit App.vue, main.js and add config.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,13 @@
 import { createApp } from 'vue'
 import router from '@/router/router'
 import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import bootstrap from 'bootstrap/dist/js/bootstrap.bundle.js'
+
 
 const app = createApp(App)
 
+// app.config.globalProperties.$axios = axios;
 app.use(router);
+app.use(bootstrap);
 app.mount("#app");
