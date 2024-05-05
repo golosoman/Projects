514b57f6c03c2126a4015727f81c56b734ed83be
Golosmanchick <constan.lapin2015@mail.ru>
2024-05-05 23:48:14 +0400

    add hooks usePosts.js and usePostById.js

---
+++ frontend/src/hooks/post/usePosts.js
@@ -0,0 +1,23 @@
+import { ref, onMounted } from 'vue'
+// import {API_PORT, API_HOST} from '@/config.js'
+import axios from 'axios';
+// import postsList from '@/data/posts.json'
+
+export const usePosts = () => {
+  const posts = ref([])
+
+  onMounted(async () => {
+    // posts.value = postsList
+    try {
+      const response = await axios.get(`/posts`)
+      posts.value = response.data;
+      console.log(posts.value, "Мои данные");
+    } catch (error) {
+      console.log("Произошла ошибка: ", error)
+    }
+  
+    // console.log(postsList, "postsList")
+  })
+  console.log(posts, "usePosts")
+  return posts
+}
