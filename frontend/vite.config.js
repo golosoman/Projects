2e1f952e80fa68dd3c1180d4684a9a641f190705
Golosmanchick <constan.lapin2015@mail.ru>
2024-05-05 23:51:54 +0400

    add new dependences to frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,16 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '^/posts': {
+        target: 'http://localhost:5000',
+        ws: true,
+        secure: false,
+        changeOrigin: true
+      }
+    }
+  },
   plugins: [vue()],
   resolve: {
     alias: {
