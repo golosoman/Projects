729a4fb2245d3e475b6ed8fef89454c4f04f6c7c
Golosmanchick <constan.lapin2015@mail.ru>
2024-05-07 23:51:53 +0400

    add usePostById, usePosts, useComments

--- frontend/src/hooks/useComments.js
+++ frontend/src/hooks/useComments.js
@@ -1,11 +1,21 @@
 import { ref, onMounted } from 'vue'
-import commentsList from '@/data/comments.json'
+import axios from '@/axiosConfig'
 
-export const useComments = () => {
+export const useComments = (id) => {
   const comments = ref([])
 
-  onMounted(() => {
-    comments.value = commentsList
+  onMounted(async () => {
+    // posts.value = postsList
+    try {
+      const response = await axios.get(`/posts/${id}/comments`)
+      
+      comments.value = response.data;
+      console.log(comments.value, "Мои комментарии");
+    } catch (error) {
+      console.log("Произошла ошибка: ", error)
+    }
+  
+    // console.log(postsList, "postsList")
   })
   return comments
 }
