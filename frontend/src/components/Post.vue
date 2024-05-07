99804fd28d3cd75b43e84d0db9e8ef728cf105a9
Golosmanchick <constan.lapin2015@mail.ru>
2024-05-07 23:52:49 +0400

    add Comment, CommentList, Loader, PostCard, Secure and edit Navbar, Post, PostList

--- frontend/src/components/Post.vue
+++ frontend/src/components/Post.vue
@@ -1,3 +1,4 @@
+<!-- eslint-disable vue/multi-word-component-names -->
 <script setup>
 import { defineProps } from 'vue'
 const props = defineProps({
@@ -9,22 +10,25 @@ const props = defineProps({
 </script>
 
 <template>
-  <div v-if="props.post.status">
-    <div class="card text-center">
-      <div class="card-header">
-        <h3>{{ props.post.title }}</h3>
-      </div>
-      <div class="card-body">
-        <p class="card-text">{{ props.post.content }}</p>
-        <a @click="$router.push(`/posts/${props.post.id}`)" class="btn btn-dark">Перейти</a>
-      </div>
-      <div class="card-footer text-muted">
-        {{ props.post.published_at }}
-      </div>
+  <div class="card mb-4">
+    <div class="card-header bg-white">
+      <div class="content p-2">
+          <h3>{{ props.post.title }}</h3>
+          <div class="d-flex flex-row align-items-center mt-3">
+              <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(4).webp" alt="avatar" width="25"
+              height="25" />
+              <p class="small mb-0 ms-2"><span>Пользователь: </span></p>
+          </div>
+          <div class="d-flex flex-row align-items-left mt-3">
+              <p class="small text-muted mb-0">Дата: {{ props.post.published_at }}</p>
+          </div>
+        </div>
+    </div>
+    <div class="card-body">
+        <p>{{ props.post.content }}</p>
     </div>
   </div>
 </template>
 
 <style scoped>
-  
 </style>
---
+++ frontend/src/components/Secure.vue
@@ -0,0 +1,6 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+    <div>
+      <h1>This page is protected by auth</h1>
+    </div>
+</template>
