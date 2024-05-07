c06c95f1d43731efed7937696882157b386839b8
Golosmanchick <constan.lapin2015@mail.ru>
2024-05-07 23:51:53 +0400

    add usePostById, usePosts, useComments

--- frontend/src/hooks/post/usePostById.js
+++ frontend/src/hooks/post/usePostById.js
@@ -1,6 +1,6 @@
 import { ref, onMounted } from 'vue'
 // import {API_PORT, API_HOST} from '@/config.js'
-import axios from 'axios';
+import axios from '@/axiosConfig'
 // import postsList from '@/data/posts.json'
 
 export const usePostById = (id) => {
@@ -11,13 +11,12 @@ export const usePostById = (id) => {
     try {
       const response = await axios.get(`/posts/${id}`)
       posts.value = response.data;
-      console.log(posts.value, "Мои данные");
+      console.log(posts.value, "Мои посты по идентификатору");
     } catch (error) {
       console.log("Произошла ошибка: ", error)
     }
   
     // console.log(postsList, "postsList")
   })
-  console.log(posts, "usePosts")
   return posts
 }
