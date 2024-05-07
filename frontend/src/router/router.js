556d45d334004a2594957f25d583f2cdd83796d5
Golosmanchick <constan.lapin2015@mail.ru>
2024-05-07 23:53:10 +0400

    edit router

--- frontend/src/router/router.js
+++ frontend/src/router/router.js
@@ -5,35 +5,63 @@ import PostIdPage from '@/pages/PostIdPage.vue'
 import PostsPage from '@/pages/PostsPage.vue'
 import RegisterPage from '@/pages/RegisterPage.vue'
 import AuthPage from '@/pages/AuthPage.vue'
+import Secure from '@/components/Secure.vue'
+import store from '@/store/index.js'
 // import PostIdPage from '@/pages/PostIdPage.vue'
 // import PostsPage from '@/pages/PostsPage.vue'
 
 const routes = [
   {
-    path: '/',
-    component: MainPage
-  },
-  {
     path: '/posts/:id',
-    component: PostIdPage
+    name: "post",
+    component: PostIdPage,
+    props: true
   },
   {
-    path: '/posts',
+    path: '/',
+    name: 'posts',
     component: PostsPage
   },
   {
     path: '/register',
+    name: 'register',
     component: RegisterPage
   },
   {
-    path: '/authorization',
+    path: '/login',
+    name: 'login',
     component: AuthPage
+  },
+  // {
+  //   path: '/rersonal-account',
+  //   name: 'p-account',
+  //   component: <template><div>Нифига себе</div></template>
+  // },
+  {
+    path: '/secure',
+    name: 'secure',
+    component: Secure,
+    meta: { 
+      requiresAuth: true
+    }
   },
 ]
 
 const router = createRouter({
   routes,
   history: createWebHistory()
+})
+
+router.beforeEach((to, from, next) => {
+  if(to.matched.some(record => record.meta.requiresAuth)) {
+    if (store.getters.isLoggedIn) {
+      next()
+      return
+    }
+    next('/login') 
+  } else {
+    next() 
+  }
 })
 
 export default router;
