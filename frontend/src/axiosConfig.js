467610a8c4e47506eb801873a83196de79989e03
Golosmanchick <constan.lapin2015@mail.ru>
2024-05-11 18:08:20 +0400

    add some dependences

--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -1,8 +1,13 @@
 import axios from 'axios'
-import {API_HOST, API_PORT} from './config'
+import { API_HOST, API_PORT } from './config'
 
 const instance = axios.create({
     baseURL: `${API_HOST}:${API_PORT}`
 })
 
-export default instance;
+instance.interceptors.request.use((config) => {
+    config.headers.Authorization = `Bearer ${localStorage.getItem('token')}`
+    return config // Путь свободен, продолжаем выполнение запроса
+})
+
+export default instance
