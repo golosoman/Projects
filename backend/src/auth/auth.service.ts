a9d5296ddfe3b52cfbea98e1693fb9dc1f34d67a
Golosmanchick <constan.lapin2015@mail.ru>
2024-05-11 18:10:36 +0400

    add getUserByToken, decodeJwtToken to service and getUser to controller

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -5,6 +5,7 @@ import * as bcrypt from 'bcryptjs'
 import { UserService } from '../users/users.service';
 import { User } from '../users/users.model';
 import { LoginUserDto } from 'src/users/dto/login-user.dto';
+import { UserDto } from 'src/users/dto/users.dto';
 
 @Injectable()
 export class AuthService {
@@ -27,11 +28,23 @@ export class AuthService {
         return this.generateToken(user)
     }
 
+    async getUserByToken(headers: Record<string, string>): Promise<UserDto> {
+        const user = this.decodeJwtToken(
+            headers["authorization"].split(" ")[1]
+        );
+        return this.userService.getOneUser(user.id)
+    }
+
     private async generateToken(user: User) {
         const payload = {email: user.email, id: user.id, roles: user.roles}
         return {
             token: this.jwtService.sign(payload)
         }
+    }
+
+    public decodeJwtToken(token: string) {
+        const payload = this.jwtService.verify(token);
+        return payload;
     }
 
     private async validateUser(userDto: LoginUserDto) {
