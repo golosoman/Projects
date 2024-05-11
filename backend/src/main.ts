53d2bf11371e83a37b847afe85468f2b287079c7
Golosmanchick <constan.lapin2015@mail.ru>
2024-05-11 18:16:26 +0400

    edit main and app-module, also add development and production configs and add some dependences

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,16 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { ValidationPipe } from './pipes/validation.pipe';
+import cors from 'cors'
 
 async function start() {
     const PORT = process.env.PORT || 5000;
 
-    const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule, { cors: true });
     
+    app.useGlobalPipes(new ValidationPipe());
+    // app.use(cors());
+
     await app.listen(PORT, () => {
         console.log(`server started in ${PORT}`);
     });
