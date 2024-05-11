0a27558cf53c07ff33b17ffb90a8392f45cc4339
Golosmanchick <constan.lapin2015@mail.ru>
2024-05-11 18:17:00 +0400

    delete

--- backend/src/user/dto/create-user.dto.ts
+++
@@ -1,9 +0,0 @@
-import { ApiProperty } from "@nestjs/swagger";
-
-export class CreateUserDto{
-    readonly name:string;
-    @ApiProperty({example: 'userExample@mail.ru', description: 'Почтовый адрес'})
-    readonly email:string;
-    @ApiProperty({example: '1234567890', description: 'Пароль'})
-    readonly password: string;
-}
