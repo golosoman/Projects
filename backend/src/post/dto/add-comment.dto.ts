40364438d7575653228623d2e0bbd955330e55a5
Golosmanchick <constan.lapin2015@mail.ru>
2024-05-11 18:12:57 +0400

    edit postcontroller, postmodule, postservice, create-post-dto, post-dto and add add-comment-dto

---
+++ backend/src/post/dto/add-comment.dto.ts
@@ -0,0 +1,3 @@
+export class AddCommentDto {
+    message: string;
+}
--- backend/src/post/post.controller.ts
+++ backend/src/post/post.controller.ts
@@ -8,20 +8,36 @@ import {
     HttpStatus,
     Patch,
     Delete,
+    UploadedFile,
+    UseInterceptors,
+    UseGuards,
+    Req,
+    Headers,
 } from "@nestjs/common";
 import { PostService } from "./post.service";
 import { PostDto } from "./dto/post.dto";
 import { CreatePostDto } from "./dto/create-post.dto";
+import { FileInterceptor } from "@nestjs/platform-express";
+import { CreateCommentDto } from "src/comment/dto/create-comment.dto";
+import { AddCommentDto } from "./dto/add-comment.dto";
+import { Roles } from "src/auth/roles-auth.decorator";
+import { RolesGuard } from "src/auth/roles.guard";
 
 @Controller("posts")
 export class PostController {
     constructor(private postService: PostService) {}
 
     @Post()
-    @HttpCode(HttpStatus.CREATED)
-    async create(@Body() createPostDto: CreatePostDto) {
-        // console.log(createPostDto);
-        return this.postService.create(createPostDto);
+    @Roles("USER", "ADMIN")
+    @UseGuards(RolesGuard)
+    @UseInterceptors(FileInterceptor("image"))
+    createPost(
+        @Body() dto: CreatePostDto,
+        @UploadedFile() image: any,
+        @Headers() headers: Record<string, string>
+    ) {
+        console.log(image);
+        return this.postService.create(dto, image, headers);
     }
 
     @Get(":id")
@@ -30,22 +46,43 @@ export class PostController {
         return this.postService.getOnePost(id);
     }
 
+    @Post(":id/comments")
+    @Roles("USER", "ADMIN")
+    @UseGuards(RolesGuard)
+    @HttpCode(HttpStatus.OK)
+    async addComment(
+        @Param("id") id: number,
+        @Body() dto: AddCommentDto,
+        @Headers() headers: Record<string, string>
+    ): Promise<AddCommentDto> {
+        return this.postService.addComment(id, dto, headers);
+    }
+
+    @Get(":id/comments")
+    @HttpCode(HttpStatus.OK)
+    async getComments(@Param("id") id: number) {
+        return this.postService.getComments(id);
+    }
+
     @Get()
     @HttpCode(HttpStatus.OK)
     async getAll(): Promise<PostDto[]> {
-      // console.log(123)
+        // console.log(123)
         return this.postService.getAllPosts();
     }
 
     @Patch(":id")
     @HttpCode(HttpStatus.OK)
-    async update(@Param("id") id: number, @Body() updatePostDto: CreatePostDto): Promise<boolean> {
-      return this.postService.updatePost(id, updatePostDto)
+    async update(
+        @Param("id") id: number,
+        @Body() updatePostDto: CreatePostDto
+    ): Promise<boolean> {
+        return this.postService.updatePost(id, updatePostDto);
     }
 
     @Delete(":id")
     @HttpCode(HttpStatus.OK)
     async delete(@Param("id") id: number): Promise<boolean> {
-      return this.postService.deletePost(id)
+        return this.postService.deletePost(id);
     }
 }
