3cbec818201b130c47ff3616657910a133979b3b
Golosmanchick <constan.lapin2015@mail.ru>
2024-05-11 18:08:20 +0400

    add some dependences

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,28 +1,26 @@
 import { createApp } from 'vue'
 import router from '@/router/router'
 import App from './App.vue'
-import 'bootstrap/dist/css/bootstrap.css'
+import "bootstrap/dist/css/bootstrap.min.css"
 import bootstrap from 'bootstrap/dist/js/bootstrap.bundle.js'
-import store from '@/store/index.js';
+// import { BAlert } from 'bootstrap-vue'
+import store from '@/store/index.js'
 import Axios from 'axios'
-// import Vue from "vue"
-
-// Vue.prototype.$http = Axios;
-
-// const token = localStorage.getItem('token')
-// if (token) {
-//   Vue.prototype.$http.defaults.headers.common['Authorization'] = token
-// }
+import components from '@/components/UI'
 
 const app = createApp(App)
 
-app.config.globalProperties.$http=Axios;
+app.config.globalProperties.$http = Axios
+
 const token = localStorage.getItem('token')
 if (token) {
-  app.config.globalProperties.$http.defaults.headers.common['Authorization'] = token
+    app.config.globalProperties.$http.defaults.headers.common['Authorization'] = token
 }
-// app.config.globalProperties.$axios = axios;
-app.use(store)
-app.use(router);
-app.use(bootstrap);
-app.mount("#app");
+
+// app.component('b-alert', BAlert)
+
+components.forEach((component) => {
+    app.component(component.name, component)
+})
+
+app.use(store).use(router).use(bootstrap).mount('#app')
