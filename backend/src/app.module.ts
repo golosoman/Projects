63eb1d279d3b159e767f9505ba216124f23c5350
Golosmanchick <constan.lapin2015@mail.ru>
2024-05-11 18:16:26 +0400

    edit main and app-module, also add development and production configs and add some dependences

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,17 +3,28 @@ import { SequelizeModule } from "@nestjs/sequelize";
 import { ConfigModule } from "@nestjs/config";
 import { Post } from "./post/post.model";
 import { Comment } from "./comment/comment.model";
-import { User } from "./user/user.model";
+import { User } from "./users/users.model";
 import { PostModule } from "./post/post.module";
-import { UserModule } from "./user/user.module";
+import { UserModule } from "./users/users.module";
 import { CommentModule } from "./comment/comment.module";
+import { AuthModule } from './auth/auth.module';
+import { RolesModule } from './roles/roles.module';
+import { UserRoles } from "./roles/user-roles.model";
+import { Role } from "./roles/roles.model";
+import { ServeStaticModule } from "@nestjs/serve-static";
+import * as path from "path";
+
+console.log(process.env.NODE_ENV)
 
 @Module({
     controllers: [],
     providers: [],
     imports: [
         ConfigModule.forRoot({
-            envFilePath: `.env`
+            envFilePath: `.${process.env.NODE_ENV}.env`
+        }),
+        ServeStaticModule.forRoot({
+            rootPath: path.resolve( __dirname, 'static'),
         }),
         SequelizeModule.forRoot({
             dialect: 'postgres',
@@ -22,12 +33,14 @@ import { CommentModule } from "./comment/comment.module";
             username: process.env.POSTGRES_USER,
             password: process.env.POSTGRES_PASSWORD,
             database: process.env.POSTGRES_DB,
-            models: [Post, Comment, User],
+            models: [Post, Comment, User, UserRoles, Role],
             autoLoadModels: true,
         }),
         PostModule,
         UserModule,
-        CommentModule
+        CommentModule,
+        AuthModule,
+        RolesModule
     ]
 })
 export class AppModule {
