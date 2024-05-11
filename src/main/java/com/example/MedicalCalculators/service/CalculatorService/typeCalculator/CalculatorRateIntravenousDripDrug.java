03cb26e519f4538b7dbaac7157c53be90c827301
Golosmanchick <constan.lapin2015@mail.ru>
2024-05-11 19:11:33 +0400

    multi-line comments have been added

--- src/main/java/com/example/MedicalCalculators/service/CalculatorService/typeCalculator/CalculatorRateIntravenousDripDrug.java
+++ src/main/java/com/example/MedicalCalculators/service/CalculatorService/typeCalculator/CalculatorRateIntravenousDripDrug.java
@@ -9,10 +9,13 @@ import java.text.DecimalFormat;
 @Log4j2
 public class CalculatorRateIntravenousDripDrug extends BaseCalculator<RIDDCalculatorRequest> {
     public CalculatorRateIntravenousDripDrug() {
-        super(CalculatorType.RATE_INTRAVENOUS_DRIP_DRUG, "*Расчет скорости внутривенного " +
-                "капельного введения препарата\nФормула: количество капель в " +
-                "минуту = V*20/t, где V - объем раствора в милилитрах, t - время в минутах, 20 - среднее " +
-                "количество капель в милилитре, v - скорость введения в каплях в минуту");
+        super(CalculatorType.RATE_INTRAVENOUS_DRIP_DRUG,
+                """
+                        *Расчет скорости внутривенного капельного введения препарата\nФормула: 
+                        количество капель в минуту = V*20/t, где V - объем раствора в милилитрах, 
+                        t - время в минутах, 20 - среднее количество капель в милилитре, 
+                        v - скорость введения в каплях в минуту
+                        """);
         log.info("CalculatorRateIntravenousDripDrug has been created");
     }
 
