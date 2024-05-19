2fe5ebd30ee28903a888a99441cf1a82c88161ea
Golosmanchick <constan.lapin2015@mail.ru>
2024-05-19 23:52:00 +0400

    edit MainPage, BMICalculatorPage, RIDDCalculatorPage, TitrationCalculatorPage

--- frontend/src/pages/MainPage.vue
+++ frontend/src/pages/MainPage.vue
@@ -1,5 +1,21 @@
 <template>
     <div class="content">
         <h1>Ну типо привет!</h1>
+        <calculator-list-card :calculators="calculatorsInfo" />
     </div>
 </template>
+<script>
+import { useCalculators } from '@/hooks/calculators/useCalculators'
+import CalculatorListCard from '@/components/CalculatorListCard.vue'
+
+export default {
+    components: {
+        CalculatorListCard
+    },
+    setup() {
+        const {calculatorsInfo} = useCalculators(3)
+        console.log(calculatorsInfo) 
+        return { calculatorsInfo }
+    }
+}
+</script>
