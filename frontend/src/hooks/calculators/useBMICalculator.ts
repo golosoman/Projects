69ee0b9d3c1cce489f5aa9fd3f468ee5c9a79ceb
Golosmanchick <constan.lapin2015@mail.ru>
2024-05-23 23:39:21 +0400

    add useBMICalculator hook

--- frontend/src/hooks/calculators/useBMICalculator.ts
+++ frontend/src/hooks/calculators/useBMICalculator.ts
@@ -1,5 +1,21 @@
 import axios from '@/axiosConfig'
+import { ref, type Ref, onMounted } from 'vue'
 
-export function useBMICalculator() {
-    // const calculator inf
+export function useBMICalculator(weight: number, height: number) {
+    const calculatorResult: Ref<number> = ref(0);
+    const getResult = async() => {
+        try {
+            const response = await axios.post('/calculator/body-mass-index/result', {
+                'weightPatient': weight,
+                'height': height
+            })
+            calculatorResult.value = response.data;
+        } catch (error) {
+            console.log(error);
+        } 
+    }
+    onMounted(() => {
+        getResult()
+    })
+    return {calculatorResult}
 }
