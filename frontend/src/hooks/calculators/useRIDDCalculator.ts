363ff46b7a5a89e76011591be85d73a4b2899601
Golosmanchick <constan.lapin2015@mail.ru>
2024-05-24 19:05:53 +0400

    added hooks

--- frontend/src/hooks/calculators/useRIDDCalculator.ts
+++ frontend/src/hooks/calculators/useRIDDCalculator.ts
@@ -1,14 +1,13 @@
 import axios from '@/axiosConfig'
-import { ref, type Ref, onUpdated} from 'vue'
 import type { ICalculatorResult } from '@/types/calculator';
-export function useBMICalculator(weight: number, height: number) {
-    // const calculatorResult: Ref<ICalculatorResult | null> = ref(null);
+export function useRIDDCalculator(volume: number, time: number) {
 
     const getResult = async() => {
         try {
-            const response = await axios.post<ICalculatorResult>('/calculator/body-mass-index/result', {
-                'weightPatient': weight,
-                'height': height
+            const response = await axios.post<ICalculatorResult>('/calculator/rate-intravenous-drip-drug/result', {
+                'volumeOfSolution': volume,
+                'timeTaking': time,
+                'isMinute': true
             })
             // calculatorResult.value = response.data;
             return response.data
