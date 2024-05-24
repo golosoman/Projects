de2c674793ed99536e7d1a897077179792f368b5
Golosmanchick <constan.lapin2015@mail.ru>
2024-05-24 19:08:46 +0400

    edit Navbar, add CalculatorCard, edit CalculatorListCard

---
+++ frontend/src/components/CalculatorListCard.vue
@@ -0,0 +1,21 @@
+<template> 
+    <div class="card-deck d-flex justify-content-around">
+        <CalculatorCard
+            v-for="calculator in calculators"
+            :calculator="calculator"
+            :key="calculator.id"
+        />
+    </div>
+</template>
+<script setup>
+import CalculatorCard from "./CalculatorCard.vue";
+import {defineProps} from "vue"
+
+const props = defineProps({
+    calculators: {
+        type: Array,
+        required: true
+    }
+})
+
+</script>
