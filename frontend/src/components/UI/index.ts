e176fa7c9be98792cd6a204b2e22b3b4bdb70db3
Golosmanchick <constan.lapin2015@mail.ru>
2024-05-24 19:07:56 +0400

    added UI components

---
+++ frontend/src/components/UI/index.ts
@@ -0,0 +1,4 @@
+import BaseButton from '@/components/UI/BaseButton.vue'
+import BaseInput from '@/components/UI/BaseInput.vue'
+import Loader from '@/components/UI/Loader.vue'
+export default [BaseButton, BaseInput, Loader]
