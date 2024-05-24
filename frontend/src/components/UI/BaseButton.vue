a2993241aac4763400aa097012a1a03a5be431e9
Golosmanchick <constan.lapin2015@mail.ru>
2024-05-24 19:07:56 +0400

    added UI components

---
+++ frontend/src/components/UI/BaseButton.vue
@@ -0,0 +1,15 @@
+<template>
+    <button data-mdb-ripple-init class="btn btn-dark btn-block">
+        <slot></slot>
+    </button>
+</template>
+<script lang="ts">
+import { Ripple, initMDB } from 'mdb-ui-kit'
+export default {
+    name: 'base-button',
+    setup() {
+        initMDB({ Ripple })
+    }
+}
+</script>
+<style scoped></style>
