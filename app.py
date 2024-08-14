318c4bc5083b99eacc57929a9d0407a21c26a11f
golosoman <constan.lapin2015@mail.ru>
2024-08-14 13:10:22 +0300

    parse dom from register page, add requirements

--- app.py
+++ app.py
@@ -1,1 +1,24 @@
-print('hello World')
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup
+
+# Настройка headless Chrome
+options = Options()
+options.add_argument('--headless')
+options.add_argument('--disable-gpu')  # Опционально: для более стабильной работы
+
+# Загрузка страницы
+driver = webdriver.Chrome(options=options)
+driver.get('https://httpwg.org/specs/')
+
+# Получение HTML-кода страницы
+html = driver.page_source
+
+# Парсинг DOM-дерева с помощью Beautiful Soup
+soup = BeautifulSoup(html, 'html.parser')
+
+# Вывод информации о DOM-дереве
+print(soup.prettify())
+
+# Закрытие браузера
+driver.quit()
